486e41e69b47ae472d13cf17dbf51fa6b2b5b389
Don Patterson <d_j_p_3@djp3.net>
2015-10-22 14:00:43 -0700

    Added chat functionality

--- my_web_server.js
+++ my_web_server.js
@@ -1,8 +1,31 @@
 var port = 8080;
-var express = require('express'),
-	app = express();
+var express = require('express');
+var	app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/public'));
+app.get('/*',function(req,res){
+	var dictionary = require('url').parse(req.url, true);
+	//console.log(dictionary.pathname);
+	res.sendFile(__dirname + '/public'+dictionary.pathname);
+});
 
-app.listen(port);
-console.log('Server running on port '+port+'.');
+io.on('connection', function(socket){
+	socket.on('chat_message', function(msg){
+		io.emit('chat_message', msg);
+	});
+});
+
+//io.on('connection',function(socket){
+//		console.log('connection established');
+//		socket.on('chat message', function(msg){
+//				console.log('message: '+msg);
+//		});
+//		socket.on('disconnect',function(){
+//				console.log('disconnected');
+//		});
+//});
+
+http.listen(port,function(){
+	console.log('Server running on port '+port+'.');
+});
