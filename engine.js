dae692a8e1ff3aa6fed82c82d960a4c36dca2482
Hunter McGushion <hunter@mcgushion.comgit>
2015-10-27 10:13:46 -0700

    makeGUID function added for "Create ID" function

--- engine.js
+++ engine.js
@@ -1,8 +1,19 @@
 // The functions the API server sees
 
 module.exports = {
-	get_time:  function(){
+	
+	get_time: function(){
 		var currentTime = new Date();
 		return currentTime.getHours().toString() + ":" + currentTime.getMinutes().toString() + ":" + currentTime.getSeconds().toString();
 	}
+	
+	var makeGUID = function() {
+    	var helper = function(s) {
+        	var p = (Math.random().toString(16)+"000000000").substr(2,8);
+        	return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
+   		}
+    //console.log(helper() + helper(true) + helper(true) + helper());
+    return helper() + helper(true) + helper(true) + helper();
+	}
+	
 };
