fd2bb48d4fffbf3faa6956bc638f98b4fd7cd6db
Don Patterson <d_j_p_3@djp3.net>
2015-10-30 17:12:37 -0700

    Committing so that I can delete it

--- my_web_server.js
+++ my_web_server.js
@@ -2,7 +2,6 @@ var port = 8080;
 var express = require('express');
 var	app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
 app.get('/*',function(req,res){
 	var dictionary = require('url').parse(req.url, true);
@@ -10,22 +9,6 @@ app.get('/*',function(req,res){
 	res.sendFile(__dirname + '/public'+dictionary.pathname);
 });
 
-io.on('connection', function(socket){
-	socket.on('chat_message', function(msg){
-		io.emit('chat_message', msg);
-	});
-});
-
-//io.on('connection',function(socket){
-//		console.log('connection established');
-//		socket.on('chat message', function(msg){
-//				console.log('message: '+msg);
-//		});
-//		socket.on('disconnect',function(){
-//				console.log('disconnected');
-//		});
-//});
-
 http.listen(port,function(){
-	console.log('Server running on port '+port+'.');
+	console.log('Web Server running on port '+port+'.');
 });
