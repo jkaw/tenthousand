f2459a1b7827fac2b9623b6cf68e3bdd11e646b5
Hunter McGushion <hunter@mcgushion.comgit>
2015-11-12 10:32:55 -0800

    no message

--- engine/our_engine.js
+++ engine/our_engine.js
@@ -34,9 +34,9 @@ module.exports = {
 	*/
 	from_api_create_game:function(user_id, turn_length) {
 		var the_game_id = call_create_ID();
-		var new_game = new game(the_game_id,
-			user_id, [user_id], turn_length, "NA", user_id, sysCurrentTime);
+		var new_game = new game(the_game_id, [user_id], turn_length, "NA", user_id);
 		console.log(the_game_id);
+		return the_game_id;
 	},
 	/*****************************************/
 
@@ -122,7 +122,7 @@ validate_ID_help = function(id){
 call_create_ID.validGames = [];
 
 // creates a new GUID and stores the new id in a static array
-function call_create_ID(){
+function call_create_ID() {
 	var id = helper_create_ID() + helper_create_ID(true) + helper_create_ID(true) + helper_create_ID();
 	call_create_ID.validGames.push(new game(id));
 	return id;
@@ -149,14 +149,11 @@ console.log(call_get_time());
 
 
 
-//Proposed game state functions and objects follow:
-
 //creates new game object
 function game(gameID, listOfPlayers, turnLimit, startTime, whoseTurn) {
 	//gameID randomly created when player creates new game, or specified when player joins game
 	this.gameID = gameID;
 	//array of all player objects within game - number of player objects created with specified gameID
-	//should be in order of time joined and will determine turn sequence?
 	this.listOfPlayers = listOfPlayers;
 	//time limit specified by gameCreator for maximum turn length
 	this.turnLimit = turnLimit;
@@ -164,14 +161,11 @@ function game(gameID, listOfPlayers, turnLimit, startTime, whoseTurn) {
 	this.startTime = startTime;
 	//playerID of the player whose turn it is now
 	this.whoseTurn = whoseTurn;
-	this.currentTime = null;
 }
 
 //creates a new player object
 function player(playerNum, playerFields) {
 	//playerID specified when player creates new game or when player joins game
-	//may need different player ID number separate from this one to allow players of same ID in different games
-	//possibly make var name concatenation of gameID and playerID
 	this.playerNum = playerNum;
 	// an array of Field objects representing the fields owned by each player
 	this.playerFields = playerFields;
@@ -179,11 +173,11 @@ function player(playerNum, playerFields) {
 
 //var name should be gameID with all dashes replaced with underscores
 var xxxxxxxx_xxxx_xxxx_xxxx_xxxxxxxxxxxx = new game("xxxxxxxx_xxxx_xxxx_xxxx_xxxxxxxxxxxx",
-	"player1", ["player1", "player2", "player3", "player4", "player5"], 3.5, "NA", "player1", sysCurrentTime);
+	"player1", ["player1", "player2", "player3", "player4", "player5"], 3.5, "NA", "player1");
 
 //var name should be gameID with all dashes replaced with underscores
 var yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy = new game("yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy",
-	"player6", ["player6"], 5.0, "NA", "player6", sysCurrentTime);
+	"player6", ["player6"], 5.0, "NA", "player6");
 
 var createGame = function(playerID, turnLimit) {
 	//will initialize a game object and transition creator to "join game" lobby,
@@ -220,11 +214,13 @@ var advanceTurn = function(game) {
 	console.log("Current player: " + currentPlayer);
 }
 
-startGame(yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy);
+//startGame(yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy);
 //fix valid id array so that id pushing does not occur only when random GUID is generated
+
+call_create_ID.validGames.push("yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy", ["player6"], 5.0, "NA", "player6");
 call_create_ID.validGames.push("yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy");
 console.log("Should be true: " + validate_ID_help("yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy"));
-yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy.listOfPlayers.push("test player");
+//yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy.listOfPlayers.push("test player");
 console.log("Test players: " + yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy.listOfPlayers);
 joinGame("new player", yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy)
 advanceTurn(yyyyyyyy_yyyy_yyyy_yyyy_yyyyyyyyyyyy);
